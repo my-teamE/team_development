7db8848e7099de8e124376b752c2e1a162272599
warabimochi <ecccompsenyou@gmail.com>
2023-07-03 12:00:22 +0900

    画像保存・DBにレコード追加

--- app/Http/Controllers/postDataController.php
+++ app/Http/Controllers/postDataController.php
@@ -4,8 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\postDataRequest;
 use Illuminate\Http\Request;
-use App\Http\Requests\Kadai05_1Request;
 use Illuminate\Support\Facades\Storage;
+use App\Models\Event;
+use Illuminate\Support\Facades\DB;
 
 class postDataController extends Controller
 {
@@ -19,7 +20,20 @@ class postDataController extends Controller
         $image = Storage::disk('public')->put('/postimages',$request->image);
         $image = basename($image);
 
-        //TODO:DB保存×
+        //TODO:DB保存〇
+        $event = new Event();
+        $event->student_id = mt_rand(1000000, 9999999);
+        $event->no = mt_rand();
+        $event->title = "テストタイトル";
+        $event->tag = "テストタグ";
+        $event->message = "テストメッセージ";
+        $event->status = mt_rand(1000, 9999);
+        $event->date = mt_rand(1, 12). "月".mt_rand(1, 30)."日";
+        //imsgeのみだとnotNull制約とdefault値Nullによってエラー吐く
+        $event->image = $image;
+        DB::transaction(function () use($event) {
+            $event->save();
+        });
 
         //CSRFトークンを破棄
         $request->session()->regenerateToken();
