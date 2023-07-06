249fb9d30ffe10dd7ecd1f1ff8068dd0353b1945
miti <2220399@ecc.ac.jp>
2023-07-06 12:14:09 +0900

    changes

--- app/Http/Controllers/TopPageController.php
+++ app/Http/Controllers/TopPageController.php
@@ -2,15 +2,36 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\account;
+use Illuminate\Contracts\Session\Session;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class TopPageController extends Controller
 {
     //
     public function index()
     {
-  return view('toppage');
+        return view('toppage');
     }
+    //ログイン機能
+    public function login(Request $request)
+    {
+        $student_id = $request->input('student_id');
+        $password = $request->input('password');
 
+        // データベースの比較
+        $account = Account::where('student_id', $student_id)
+            ->where('password', $password)
+            ->first();
+
+        if ($account) {
+            Auth::login($account);
+            return view('toppage', compact('account'));
+        }
+    }
+    // 認証しているユーザーを取得する
+    // $user = Auth::user();
+    // 認証しているユーザーのIDを取得する
+    // $user_id = Auth::id();
 }
-
---
+++ app/Models/Account.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Contracts\Auth\Authenticatable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+
+class Account extends Model implements Authenticatable
+{
+    use HasFactory;
+
+    // Authenticatableメソッドの実装
+    public function getAuthIdentifierName()
+    {
+        return 'id';
+    }
+
+    public function getAuthIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getAuthPassword()
+    {
+        return $this->password;
+    }
+
+    public function getRememberToken()
+    {
+        return $this->remember_token;
+    }
+
+    public function setRememberToken($value)
+    {
+        $this->remember_token = $value;
+    }
+
+    public function getRememberTokenName()
+    {
+        return 'remember_token';
+    }
+}
