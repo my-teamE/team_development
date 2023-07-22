692ff4a7054bc1431e119d46911b852300999969
Nostv <sonyuu2x@gmail.com>
2023-07-23 03:43:57 +0900

    change

--- resources/views/apply.blade.php
+++ resources/views/apply.blade.php
@@ -10,38 +10,37 @@
                 <button class="btn button-close ">一覧へもどる</button>
             </a>
             </div> --}}
-            <div class="detail-layout flex mt-3">
-                <div>
-                <img class="photo-detail photo-1" src="{{ asset('/storage/postimages/'). "/" . $event->image }}" alt="" />
+            <div class="detail-layout flex mt-3 ">
+                <div class="image-container border border-gray-300 rounded-lg overflow-hidden" style="width:650px; height:400px">
+                    <img class="w-full h-full object-cover" src="{{ asset('/storage/postimages/'). "/" . $event->image }}"alt="" />
                 <br>
-                </div>
-                <div class="information flex-wrap" style="padding: 10px 0 10px 20px">
-                    <div class="bg-yellow" style="height:70%">
-                        <!-- タイトル -->
-                        <p class="text-3xl" style="color:red" >{{ $event->title }}</p>
-                        <br>
 
-                        <!-- 本文 -->
-                        <p class="i-desc"style="font-size:large">
-                            {{ $event->message }}
-                        </p>
+                </div>
+                <div class="information flex-wrap" style="padding: 10px 0 10px 20px;width:40%">
+                    <div class="bg-yellow ml-5 mb-2 flex flex-col" style="height:70%;">
+                      <!-- タイトル -->
+                      <h1 class="text-3xl text-center" style="color:white">{{ $event->title }}</h1>
+                      <br>
+
+                      <!-- 本文 -->
+                      <p class="i-desc ml-2"style="font-size:large">
+                          {{ $event->message }}
+                      </p>
+                      <p class="mt-auto italic text-l text-end">by {{ $account->name }}</p>
                     </div>
                     <!-- 送信ボタン -->
-                    <div class="subbt text-end " style="">
-                    <!-- 発注者 -->
-                        <p class="client ">{{ $account->name }}</p>
-                        <a class="btn btn-custom01" href="###">
-                            <span class="btn-custom01-front">応募する！！</span>
-                        </a>
+                    <div class="subbt text-end">
+                      <!-- 発注者 -->
+                      <a class="btn btn-custom01" href="###">
+                          <span class="btn-custom01-front">応募する！！</span>
+                      </a>
                     </div>
-                </div>
-        </div>
+                  </div>
 
-        <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
-            <a href="#"><i class="far fa-comment-dots text-yellow hover:text-blue-600" style='font-size:24px; padding:5px '></i></a>
-            <a href="{{ route('profile') }}" >
-                <i class="fas fa-user text-yellow hover:text-yellow-600" style="font-size: 26px;"></i>
-            </a>
+        </div>
+        <br>
+                <p class="text-lg ">開催場所：ECCコンピューター専門学校</p>
+                <p class="text-lg ">日付など：xx/xx/xx -xx/xx/xx</p>
         </main>
 </section>
 
