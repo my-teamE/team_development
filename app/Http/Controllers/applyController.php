ae8fff472232eea2901083baf1f24f524fdd58d9
miti <2220399@ecc.ac.jp>
2023-07-26 18:44:31 +0900

    一応完成

--- app/Http/Controllers/applyController.php
+++ app/Http/Controllers/applyController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use App\Models\Event;
 use App\Models\Account;
 use App\Models\Applydata;
+use App\Models\Profile;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 
@@ -14,11 +15,11 @@ class applyController extends Controller
 {
     // 雛形
 
-    
 
-    public function index(){
-        return view ('apply');
 
+    public function index()
+    {
+        return view('apply');
     }
 
     // クエリ文字列によるルーティング
@@ -34,8 +35,9 @@ class applyController extends Controller
 
     // 応募ボタンクリック時のエンドポイント
 
-    public function applied($id) {
-
+    public function applied($id)
+    {
+        $eventRecords = Event::all();
         $applydata = new Applydata();
         $eventStundent = Event::where('id', $id)->first();
 
@@ -50,6 +52,25 @@ class applyController extends Controller
             $applydata->save();
         });
 
-        return redirect("/toppage");
+        $student_id = $applydata->student_id;
+
+        $apply_user_id = $applydata->apply_user_code;
+
+
+        Profile::where('student_id', '=', $student_id)
+            ->update([
+                'message' => $apply_user_id . 'が応募しました',
+            ]);
+        Profile::where('student_id', '=', $apply_user_id)
+            ->update([
+                'message' => '応募完了しました',
+            ]);
+
+
+        $profile = Profile::where('student_id', '=', Auth::user()->student_id)->first();
+
+
+        // dd($message);
+        return view("/toppage", compact('profile', 'eventRecords'));
     }
 }
