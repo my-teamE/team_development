510f62efd7795aae49082e5a87465fed35e35ec9
Nostv <sonyuu2x@gmail.com>
2023-07-26 14:14:36 +0900

    change top

--- resources/views/toppage.blade.php
+++ resources/views/toppage.blade.php
@@ -6,99 +6,78 @@
         <div class="split-container">
             <div class="location-contr" id="" style="padding:20px 0px">
                 <div id="location" class="text-center py-3">
-
-                    <a href="{{ route('search') }}">
-                        <button class="bg-gray-100 hover:bg-blue-200 focus:bg-blue-300"
-                            style="border:solid rgb(69, 41, 134)">ハッカソン</button>
-                    </a>
-                    <a href="https://techplay.jp/event"><button class="bg-gray-100 hover:bg-yellow-100 focus:bg-yellow-200"
-                            style="border:solid rgb(155, 200, 67)">イベント</button></a>
-                    <a href=""><button class="bg-gray-100 hover:bg-red-200 focus:bg-red-300"
-                            style="border:solid rgb(183, 68, 68)">WEB</button></a>
+                    <button class="btsearh bg-gray-100 hover:bg-blue-200 focus:bg-blue-300" data-target="view-index" style="border:solid rgb(69, 41, 134)">ハッカソン</button>
+                    <button class="btsearh bg-gray-100 hover:bg-yellow-100 focus:bg-yellow-200" data-target="view-index" style="border:solid rgb(155, 200, 67)">イベント</button>
+                    <button class="btsearh bg-gray-100 hover:bg-red-200 focus:bg-red-300" data-target="view-index" style="border:solid rgb(183, 68, 68)">WEB</button>
                 </div>
                 <div id="location" class="text-center py-3">
-                    <a href=""><button
-                            class="bg-gray-100 hover:bg-blue-200 focus:bg-blue-300"style="border:solid rgb(69, 41, 134)">メンバー募集</button></a>
-                    <a href=""><button
-                            class="bg-gray-100 hover:bg-yellow-100 focus:bg-yellow-200"style="border:solid rgb(155, 200, 67)">アイデア募集</button></a>
-                    <a href="{{ route('hoge') }}"><button
-                            class="bg-gray-100 hover:bg-red-200 focus:bg-red-300"style="border:solid  rgb(183, 68, 68)">発注者はこちら</button></a>
+                    <button class="btsearh bg-gray-100 hover:bg-blue-200 focus:bg-blue-300" data-target="view-index" style="border:solid rgb(69, 41, 134)">メンバー募集</button>
+                    <button class="btsearh bg-gray-100 hover:bg-yellow-100 focus:bg-yellow-200" data-target="view-index" style="border:solid rgb(155, 200, 67)">アイデア募集</button>
+                    <a href="{{ route('hoge') }}"><button class="bg-gray-100 hover:bg-red-200 focus:bg-red-300" style="border:solid  rgb(183, 68, 68)">過去の制作物</button></a>
+                </div>
+           </div>
 
+            <div class="container1 p-2 " style="background: rgb(38, 36, 36)">
+                <p style="font-size: 25px; padding-left:25px">過去の代表制作品</p>
+                <div class="slider flex">
+                        <div class="slick-img">
+                            <img src="{{ asset('/storage/images/project1.png')}}" alt="Image"/>
+                        </div>
+                        <div class="slick-img">
+                            <a href=""><img src="{{ asset('/storage/images/project2.jpg')}}" alt="Image"/></a>
+                        </div>
+                        <div class="slick-img">
+                            <a href=""><img src="{{ asset('/storage/images/project3.jpg')}}" alt="Image"/></a>
+                        </div>
+                        <div class="slick-img">
+                            <a href=""><img src="{{ asset('/storage/images/student1.jpg')}}" alt="Image"/></a>
+                        </div>
+                        <div class="slick-img">
+                            <a href=""><img src="{{ asset('/storage/images/aaa.jpg')}}" alt=""/></a>
+                        </div>
+                        <div class="slick-img">
+                            <a href=""><img src="{{ asset('/storage/images/20210311134726.jpg')}}" alt=""/></a>
+                        </div>
+                </div>
+            </div>
+        </div>
+        <div class="flex">
+            <h4 class="title" style="font-size:30px;padding:2% 0;width:30%">By the community, for the community </h4>
+            {{-- 検索 --}}
+            <div class="relative text-end pt-7" style="width:70%;">
+                <input
+                    class="bg-gray-300 px-3 py-1.5 focus:outline-none focus:ring-red-500 focus:border-red-500 rounded-md text-black "
+                    style="width:36%" type="search" id="search" placeholder="検索キーワード">
+                {{-- <input class="border border-gray-300 rounded-md py-2 px-4 focus:outline-none focus:ring-blue-500 focus:border-blue-500 text-black" style="width:40%" type="search" placeholder=" 検索"> --}}
+                <button id="searchbt" class="bg-blue-300 rounded-md text-white w-10 py-1.5 ">
+                    <i class="fa fa-search"></i>
+                </button>
+            </div>
+        </div>
+        <section class="grid grid-cols-1 lg:grid-cols-4 gap-2 " id="view-index">
 
-                    <div>
-
-                        <div class="container1 p-2 " style="background: rgb(38, 36, 36)">
-                            <p style="font-size: 25px; padding-left:25px">過去の代表制作品</p>
-                            <div class="slider flex">
-                                <div class="slick-img">
-                                    <img src="{{ asset('/storage/images/project1.png') }}" alt="Image" />
-                                </div>
-                                <div class="slick-img">
-                                    <a href=""><img src="{{ asset('/storage/images/project2.jpg') }}"
-                                            alt="Image" /></a>
-                                </div>
-                                <div class="slick-img">
-                                    <a href=""><img src="{{ asset('/storage/images/project3.jpg') }}"
-                                            alt="Image" /></a>
-                                </div>
-                                <div class="slick-img">
-                                    <a href=""><img src="{{ asset('/storage/images/student1.jpg') }}"
-                                            alt="Image" /></a>
-                                </div>
-                                <div class="slick-img">
-                                    <a href=""><img src="{{ asset('/storage/images/aaa.jpg') }}"
-                                            alt="" /></a>
-                                </div>
-                                <div class="slick-img">
-                                    <a href=""><img src="{{ asset('/storage/images/20210311134726.jpg') }}"
-                                            alt="" /></a>
+            <h1 class="text-lg" id="a">検索したプロジェクトはございません！！！</h1>
+            @foreach ($eventRecords as $record)
+                <!-- 一覧画面 -->
+                <article class="photo-list row-span-2 bg-black-100 hover:bg-black-300 rounded-lg shadow-md hover:shadow-lg transition-shadow">
+                    <a href="{{ asset('/apply/'. $record->id) }}" class="photo-link ">
+                        <img class="photo-thumb photo-1 "
+                        src="{{ asset('/storage/postimages/' . $record->image) }}" alt="" width="640"
+                        height="360" alt="button-open" />
+                    </a>
+                        <div class="photo-meta ">
+                            <p class="title text-yellow-500 text-2xl ">{{ $record->title }} </p>
+                            <div class="flex">
+                                <div class="user font-bold mb-2" style="width:90%"><a href="">{{ $record->name }}</a></div>
+                                <div>
+                                    <i id="heartIcon" class="fas fa-heart text-end cursor-pointer" style="width: 20px;color:white"></i>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="flex">
-                        <h4 class="title" style="font-size:30px;padding:2% 0;width:30%">By the community, for the community
-                        </h4>
-                        {{-- 検索 --}}
-                        <div class="relative text-end pt-7" style="width:70%;">
-                            <input
-                                class="bg-gray-300 px-3 py-1.5 focus:outline-none focus:ring-red-500 focus:border-red-500 rounded-md text-black "
-                                style="width:36%" type="search" id="search" placeholder="検索キーワード">
-                            {{-- <input class="border border-gray-300 rounded-md py-2 px-4 focus:outline-none focus:ring-blue-500 focus:border-blue-500 text-black" style="width:40%" type="search" placeholder=" 検索"> --}}
-                            <button id="searchbt" class="bg-blue-300 rounded-md text-white w-10 py-1.5 ">
-                                <i class="fa fa-search"></i>
-                            </button>
-                        </div>
-                    </div>
-                    <section class="grid grid-cols-1 lg:grid-cols-4 gap-2 " id="view-index">
-
-                        <h1 class="text-lg" id="a">検索したプロジェクトはございません！！！</h1>
-                        @foreach ($eventRecords as $record)
-                            <!-- 一覧画面 -->
-                            <article
-                                class="photo-list row-span-2 bg-black-100 hover:bg-black-300 rounded-lg shadow-md hover:shadow-lg transition-shadow">
-                                <a href="{{ asset('/apply/' . $record->id) }}" class="photo-link ">
-                                    <img class="photo-thumb photo-1 "
-                                        src="{{ asset('/storage/postimages/' . $record->image) }}" alt=""
-                                        width="640" height="360" alt="button-open" />
-                                </a>
-                                <div class="photo-meta ">
-                                    <p class="title text-yellow-500 text-2xl ">{{ $record->title }} </p>
-                                    <div class="flex">
-                                        <div class="user font-bold mb-2" style="width:90%"><a
-                                                href="">{{ $record->name }}</a>
-                                        </div>
-                                        <div>
-                                            <i id="heartIcon" class="fas fa-heart text-end cursor-pointer"
-                                                style="width: 20px;color:white"></i>
-                                        </div>
-                                    </div>
-                                </div>
-                                <p class="text-gray-400 text-xs text-end pr-4 pb-2"><time
-                                        datetime="投稿記事の⽇時">2023/07/26</time></p>
-                            </article>
-                        @endforeach
-                    </section>
+                        <p class="text-gray-400 text-xs text-end pr-4 pb-2"><time datetime="投稿記事の⽇時">2023/07/26</time></p>
+                 </article>
+            @endforeach
+        </section>
     </main>
     {{-- お知らせ機能 --}}
     <div class="notification-container hidden absolute top-1/4 left-1/3 right-1/3 px-4 bg-black rounded-lg shadow-md border border-gray-300 animate__animated"
@@ -109,28 +88,27 @@
     <div class="hidden fixed top-0 right-0  sm:block z-10">
         {{-- login 画面 --}} {{-- login中の時 --}}
         @auth
-            <div class=" text-right flex">
-                <form action="{{ route('profile') }}" method="get">
-                    @csrf
-                    <button id="btm" class="text-xl" title='Profile'>
-                        <i class="menu fas fa-user text-yellow hover:text-gray-100 " style="padding:5px"></i>
-                    </button>
-                </form>
-                {{-- <button id="btm" class="group-hover:opacity-1">
+        <div class=" text-right flex">
+            <form action="{{ route('profile') }}" method="get">
+                @csrf
+                <button id="btm" class="text-xl" title='Profile'>
+                    <i class="menu fas fa-user text-yellow hover:text-gray-100 " style="padding:5px"></i>
+                </button>
+              </form>
+            {{-- <button id="btm" class="group-hover:opacity-1">
                 <i class='fas fa-bell text-yellow hover:text-gray-100' id="showNotificationButton" style='padding:5px'></i>
             </button> --}}
-                <button id="btm" title="通知">
-                    <i class='fas fa-bell text-yellow hover:text-gray-100'
-                        id="showNotificationButton"style='padding:5px'></i>
-                </button>
-                <button id="btm" title="Message">
-                    <i class="far fa-comment-dots hover:text-gray-100" id="showNotificationButton" style='padding:5px'></i>
-                </button>
-                <form action="{{ route('logout') }}" method="get" id="btm">
-                    @csrf
-                    <button class="text-lg" style="padding-top: 1px" title="Logout">Logout </button>
-                </form>
-            </div>
+                 <button id="btm" title="通知">
+                <i class='fas fa-bell text-yellow hover:text-gray-100' id="showNotificationButton"style='padding:5px'></i>
+                 </button>
+            <button id="btm" title="Message">
+                <i class="far fa-comment-dots hover:text-gray-100" id="showNotificationButton" style='padding:5px'></i>
+            </button>
+            <form action="{{ route('logout') }}" method="get" id="btm" >
+                @csrf
+                <button class="text-lg" style="padding-top: 1px" title="Logout">Logout </button>
+              </form>
+        </div>
             {{-- <div class="flex">
                 <div style="width:50%">
 
